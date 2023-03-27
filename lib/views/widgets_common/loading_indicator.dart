2494622babf3bf3ea58612bc2199840642a4f493
JAEEUN SHIN <122805045+JaeeunSkywalker@users.noreply.github.com>
2023-03-28 05:55:29 +0900

    Delete lib directory

--- lib/views/widgets_common/loading_indicator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:ecommerce_app/consts/consts.dart';
-import 'package:flutter/material.dart';
-
-Widget loadingIndicator() {
-  return const CircularProgressIndicator(
-    valueColor: AlwaysStoppedAnimation(redColor),
-  );
-}
